5401dbf413e7ee75a1d512fe723030ce934e2fb6
mhmetglrq <mhmetglr.q@gmail.com>
2024-10-17 17:49:59 +0300

    Refactor import statements for local onboard and local text repositories Add routes for Text Repeater, Text Randomizer, Word Cloud, Text Sorting, and Reverse Text

--- lib/config/router/route_names.dart
+++ lib/config/router/route_names.dart
@@ -2,4 +2,9 @@ class RouteNames {
   RouteNames._();
   static const String home = '/home';
   static const String onboard = '/onboard';
+  static const String textRepeater = '/text-repeater';
+  static const String textRandomizer = '/text-randomizer';
+  static const String wordCloud = '/word-cloud';
+  static const String textSorting = '/text-sorting';
+  static const String reverseText = '/reverse-text';
 }
