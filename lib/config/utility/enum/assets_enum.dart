aebfd21bd8616e8a690cba156bdfd7be3eb2fe0d
mhmetglrq <mhmetglr.q@gmail.com>
2024-11-04 17:55:29 +0300

    Add new dependencies and rename image_enum to assets_enum with additional JSON support

--- lib/config/utility/enum/assets_enum.dart
+++ lib/config/utility/enum/assets_enum.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-enum ImageEnum {
+enum AssetsEnum {
   messageBox('message_box'),
   menu('menu'),
   notification('notification'),
   ellipse('ellipse'),
+  empty('empty'),
   ;
 
   final String value;
-  const ImageEnum(this.value);
+  const AssetsEnum(this.value);
 
   String get toPng => 'assets/images/$value.png';
   String get toSvg => 'assets/svg/$value.svg';
+  String get toJson => 'assets/json/$value.json';
 
   Image get toPngImage => Image.asset(toPng);
   SvgPicture get toSvgImage => SvgPicture.asset(toSvg);
