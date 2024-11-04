3ab7020db2754e0c8ea5d13ce868555d302981ce
mhmetglrq <mhmetglr.q@gmail.com>
2024-11-04 17:56:07 +0300

    Add word cloud creation and saved texts retrieval to LocalTextBloc

--- lib/features/presentation/bloc/text/local/local_text_bloc.dart
+++ lib/features/presentation/bloc/text/local/local_text_bloc.dart
@@ -1,9 +1,12 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:text_repeater/features/domain/usecases/text/local/create_word_cloud_usecase.dart';
 import 'package:text_repeater/features/domain/usecases/text/local/randomize_text_usecase.dart';
 import 'package:text_repeater/features/domain/usecases/text/local/repeat_text_usecase.dart';
 import 'package:text_repeater/features/domain/usecases/text/local/reverse_text_usecase.dart';
 
+import '../../../../../config/models/text_model.dart';
+import '../../../../domain/usecases/text/local/get_saved_texts_usecase.dart';
 import '../../../../domain/usecases/text/local/sort_text_usecase.dart';
 
 part 'local_text_event.dart';
@@ -14,14 +17,23 @@ class LocalTextBloc extends Bloc<LocalTextEvent, LocalTextState> {
   final RandomizeTextUsecase _randomizeTextUsecase;
   final SortTextUsecase _sortTextUsecase;
   final ReverseTextUsecase _reverseTextUsecase;
-  LocalTextBloc(this._repeatTextUsecase, this._randomizeTextUsecase,
-      this._sortTextUsecase, this._reverseTextUsecase)
-      : super(const LocalTextInitialState()) {
+  final CreateWordCloudUseCase _createWordCloudUseCase;
+  final GetSavedTextsUsecase _getSavedTextsUsecase;
+  LocalTextBloc(
+    this._repeatTextUsecase,
+    this._randomizeTextUsecase,
+    this._sortTextUsecase,
+    this._reverseTextUsecase,
+    this._createWordCloudUseCase,
+    this._getSavedTextsUsecase,
+  ) : super(const LocalTextInitialState()) {
     on<RepeatTextEvent>(onRepeatText);
     on<RemoveTextEvent>(onRemoveText);
     on<RandomizeTextEvent>(onRandomizeText);
     on<SortTextEvent>(onSortingText);
     on<ReverseTextEvent>(onReverseText);
+    on<WordCloudEvent>(onCreateWordCloud);
+    on<GetSavedTextsEvent>(onGetSavedTexts);
   }
 
   void onRepeatText(RepeatTextEvent event, Emitter<LocalTextState> emit) async {
@@ -30,7 +42,7 @@ class LocalTextBloc extends Bloc<LocalTextEvent, LocalTextState> {
             params: RepeatTextParams(
                 text: event.text, times: event.times, newLine: event.newLine))
         .then((text) {
-      emit(LocalTextSuccessState(text: text));
+      emit(LocalTextSuccessState(text: text, savedTexts: state.savedTexts));
     }).catchError((error) {
       emit(LocalTextErrorState(message: error.toString()));
     });
@@ -67,6 +79,28 @@ class LocalTextBloc extends Bloc<LocalTextEvent, LocalTextState> {
       emit(LocalTextSuccessState(text: text));
     }).catchError((error) {
       emit(LocalTextErrorState(message: error.toString()));
+    });
+  }
+
+  void onCreateWordCloud(
+      WordCloudEvent event, Emitter<LocalTextState> emit) async {
+    emit(const LocalTextLoadingState());
+    await _createWordCloudUseCase(params: event.text).then((wordCloudList) {
+      emit(LocalTextSuccessState(wordCloudList: wordCloudList));
+    }).catchError((error) {
+      emit(LocalTextErrorState(message: error.toString()));
+    });
+  }
+
+  void onGetSavedTexts(
+      GetSavedTextsEvent event, Emitter<LocalTextState> emit) async {
+    emit(const LocalTextSavedTextsLoadingState());
+    await _getSavedTextsUsecase().then((savedTexts) {
+      emit(LocalTextSavedTextsSuccessState(
+        savedTexts: savedTexts.reversed.toList(),
+      ));
+    }).catchError((error) {
+      emit(LocalTextSavedTextsErrorState(message: error.toString()));
     });
   }
 }
--- lib/features/presentation/bloc/text/local/local_text_state.dart
+++ lib/features/presentation/bloc/text/local/local_text_state.dart
@@ -1,9 +1,12 @@
 part of 'local_text_bloc.dart';
 
 sealed class LocalTextState extends Equatable {
-  const LocalTextState({this.text, this.message});
+  const LocalTextState(
+      {this.text, this.message, this.wordCloudList, this.savedTexts});
   final String? text;
   final String? message;
+  final List<Map<String, dynamic>>? wordCloudList;
+  final List<TextModel>? savedTexts;
 
   @override
   List<Object?> get props => [
@@ -21,9 +24,22 @@ final class LocalTextLoadingState extends LocalTextState {
 }
 
 final class LocalTextSuccessState extends LocalTextState {
-  const LocalTextSuccessState({super.text});
+  const LocalTextSuccessState(
+      {super.text, super.wordCloudList, super.savedTexts});
 }
 
 final class LocalTextErrorState extends LocalTextState {
   const LocalTextErrorState({super.message});
+}
+
+final class LocalTextSavedTextsLoadingState extends LocalTextState {
+  const LocalTextSavedTextsLoadingState({super.savedTexts});
+}
+
+final class LocalTextSavedTextsSuccessState extends LocalTextState {
+  const LocalTextSavedTextsSuccessState({super.savedTexts});
+}
+
+final class LocalTextSavedTextsErrorState extends LocalTextState {
+  const LocalTextSavedTextsErrorState({super.message});
 }
